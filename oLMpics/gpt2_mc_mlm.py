e957a5e915ee4a3b1819305efb4071990d58ef01
Nammu <namratashivagunde@gmail.com>
2021-10-25 14:37:40 -0400

    fix

--- oLMpics/gpt2_mc_mlm.py
+++ oLMpics/gpt2_mc_mlm.py
@@ -454,7 +454,7 @@ def zero_shot_evaluation_mc_mlm(dataset_dict, dataset_dict_seq,  model_name, res
 
               result_new = {'model_name': model_name, 'task_name':task_name, 'accuracy_5_runs':str(np.array(accuracy)), 'accuracy_mean': np.array(accuracy).mean()*100, 'CI':-1* np.array(accuracy).mean()*100+maxi*100, 'accuracy_min':mini, 'accuracy_max':maxi  }
               results_seq = results_seq.append(result_new, ignore_index=True)
-            return results_seq
+        return results_seq
 
 results = zero_shot_evaluation_mc_mlm(dataset_dict, dataset_dict_seq, model_name_or_path, results, results_seq, seq_flag)
 
