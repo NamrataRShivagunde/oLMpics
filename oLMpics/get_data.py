3b88e2c92ca3827f537b7388cde48e6d4ee20771
Nammu <namratashivagunde@gmail.com>
2021-10-25 10:26:50 -0400

    added get_data

---
+++ oLMpics/get_data.py
@@ -0,0 +1,12 @@
+
+!wget https://olmpics.s3.us-east-2.amazonaws.com/challenge/number_comparison/number_comparison_age_compare_masked_dev.jsonl.gz
+!wget https://olmpics.s3.us-east-2.amazonaws.com/challenge/antonym_synonym_negation/antonym_synonym_negation_dev.jsonl.gz 
+!wget https://olmpics.s3.us-east-2.amazonaws.com/challenge/size_comparison/size_comparison_dev.jsonl.gz
+!wget https://olmpics.s3.us-east-2.amazonaws.com/challenge/compositional_comparison/compositional_comparison_dev.jsonl.gz
+!wget https://olmpics.s3.us-east-2.amazonaws.com/challenge/coffee_cats_quantifiers/coffee_cats_quantifiers_dev.jsonl.gz
+
+!gunzip number_comparison_age_compare_masked_dev.jsonl.gz
+!gunzip antonym_synonym_negation_dev.jsonl.gz
+!gunzip size_comparison_dev.jsonl.gz
+!gunzip compositional_comparison_dev.jsonl.gz
+!gunzip coffee_cats_quantifiers_dev.jsonl.gz
